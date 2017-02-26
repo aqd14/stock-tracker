b9e1e36a2aab1231eac09ef979f605783ba536e7
aqd14 <aqd14@msstate.edu>
2017-02-25 23:06:46 -0600

    Add app icon

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 	
 public class MainApp extends Application {
@@ -19,6 +20,7 @@ public class MainApp extends Application {
         stage.setTitle("Stock Tracker");
         stage.setScene(new Scene(root));
         stage.setResizable(false);
+        stage.getIcons().add(new Image("main/resources/images/msu_icon.jpg"));
         stage.show();
     }
 }
